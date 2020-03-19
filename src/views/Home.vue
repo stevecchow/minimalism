2ef7871b17ea42c357df471cb3807fa7cb422e87
周泓名 <zhouhongming@mshare.cn>
2020-03-19 17:51:05 +0800

    配置 vuex、data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <div class="home"></div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld
-  }
+  components: {},
+  methods: {
+    // getArr() {
+    //   let arr = [[1, 2, 3], ["x", "y", "z"], 3, 4, 5, 6, 7, 8, 9, 0, 1];
+    //   let arrFinal = [];
+    //   this.spliceArr(arr, arrFinal);
+    //   console.log("arrFinal >>> ", arrFinal);
+    // },
+    // spliceArr(input, output) {
+    //   output.push(input.splice(0, 8));
+    //   if (input.length > 0) {
+    //     this.spliceArr(input, output);
+    //   }
+    // }
+  },
+  created() {}
 };
 </script>
