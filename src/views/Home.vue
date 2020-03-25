7033645e0ca5e007f60f3b2abbfe52a598ea960b
stevechow <zhmnda@icloud.com>
2020-03-25 11:36:47 +0800

    删除about页面

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home"></div>
+  <div class="home">hello home</div>
 </template>
 
 <script>
@@ -8,20 +8,7 @@
 export default {
   name: "Home",
   components: {},
-  methods: {
-    // getArr() {
-    //   let arr = [[1, 2, 3], ["x", "y", "z"], 3, 4, 5, 6, 7, 8, 9, 0, 1];
-    //   let arrFinal = [];
-    //   this.spliceArr(arr, arrFinal);
-    //   console.log("arrFinal >>> ", arrFinal);
-    // },
-    // spliceArr(input, output) {
-    //   output.push(input.splice(0, 8));
-    //   if (input.length > 0) {
-    //     this.spliceArr(input, output);
-    //   }
-    // }
-  },
+  methods: {},
   created() {}
 };
 </script>
---
+++ src/views/Test.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="test">
+    hello test
+  </div>
+</template>
+<style lang="scss" scoped>
+.test {
+}
+</style>
